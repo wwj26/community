5ef3510c34d3214fc79211df5648cdd82af33139
zhouzao <1176901170@qq.com>
2020-08-22 17:26:43 +0800

    add index and bootstrp

---
+++ src/main/java/com/hard/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.hard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    public String test(){
+        return "index.html";
+    }
+}
